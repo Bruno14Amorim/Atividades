e77461593779f5135506fc3341f6a116d099d84d
GabrielLuizFonseca <bielnfonseca@gmail.com>
2020-03-18 21:58:26 -0300

    m

---
+++ BrincandoApplication/src/brincandoapplication/BrincandoApplication.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brincandoapplication;
+
+/**
+ *
+ * @author robso
+ */
+public class BrincandoApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+fill(224, 192, 33);//queijo
+triangle(240,257,141,75,57,207);//queijo
+fill(222, 49, 132);//uva
+ellipse(294,144,22,20);
+ellipse(313,148,22,20);
+ellipse(276,142,22,20);
+ellipse(280,159,22,20);
+ellipse(298,161,22,20);
+ellipse(288,177,22,20);
+stroke(26, 22, 26);
+fill(31, 21, 31);
+rect(185,60,46,49);
+ellipse(235,86,22,20);
+
+    }
+    
+}
